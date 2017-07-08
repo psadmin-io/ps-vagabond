b4557b93cac39e9ff42310155a98091063d9b08f
Dan Iverson <dan@daniverson.com>
2017-07-08 07:20:29 -0500

    Change regex for site.pp file

--- scripts/provision-bootstrap-ptp.ps1
+++ scripts/provision-bootstrap-ptp.ps1
@@ -57,7 +57,7 @@ $computername = $env:computername
 function change_to_midtier() {
   Write-Host "[${computername}][Task] Change env_type to 'midtier'"
   (Get-Content "${PUPPET_HOME}\data\defaults.yaml").replace("env_type: fulltier", "env_type: midtier") | Set-Content "${PUPPET_HOME}\data\defaults.yaml"
-  (Get-Content "${PUPPET_HOME}\manifests\site.pp").replace('include\s*(.+)', "include ::pt_role::pt_tools_deployment") | Set-Content "${PUPPET_HOME}\manifests\site.pp"
+  (Get-Content "${PUPPET_HOME}\manifests\site.pp") -replace 'include.*', "include ::pt_role::pt_tools_deployment" | Set-Content "${PUPPET_HOME}\manifests\site.pp"
   Write-Host "[${computername}][Done] Change env_type to 'midtier'"
 }
 
