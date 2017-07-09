ecdb2cb4ed9aece04c6d3639d142e133a1afb788
Dan Iverson <dan@daniverson.com>
2017-07-09 13:11:48 -0400

    Remove test-path for registry change

--- scripts/provision-bootstrap-ptp.ps1
+++ scripts/provision-bootstrap-ptp.ps1
@@ -58,15 +58,13 @@ function remove_from_PATH() {
   [CmdletBinding()]
     Param ( [String]$RemovedFolder )
   # Get the Current Search Path from the environment keys in the registry
-  if (Test-Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment") {
-    $NewPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-    # Find the value to remove, replace it with $NULL. If it’s not found, nothing will change.
-    $NewPath=$NewPath -replace $RemovedFolder,$NULL
-    # Update the Environment Path
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
-    # Show what we just did
-    # Return $NewPath
-  }
+  $NewPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+  # Find the value to remove, replace it with $NULL. If it’s not found, nothing will change.
+  $NewPath=$NewPath -replace $RemovedFolder,$NULL
+  # Update the Environment Path
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+  # Show what we just did
+  # Return $NewPath
 }
 
 function change_to_midtier() {
