42ee9967a91f25e9277fd80908d6d069418627fb
Dan Iverson <dan@daniverson.com>
2020-09-02 14:05:05 -0500

    Change URL for SSO change

--- scripts/provision-download.ps1
+++ scripts/provision-download.ps1
@@ -180,7 +180,7 @@ function install_additional_packages {
 
 function create_authorization_cookie {
   if (Test-Path $COOKIE_FILE) { Remove-Item $COOKIE_FILE }
-  $MOS_TOKEN = ([System.Uri](((Invoke-WebRequest -Uri "https://updates.oracle.com/Orion/SimpleSearch" `
+  $MOS_TOKEN = ([System.Uri](((Invoke-WebRequest -Uri "https://updates.oracle.com/Orion/Services/download" `
                                                 -UserAgent "Mozilla/5.0" `
                                                 -UseBasicParsing `
                                                 -MaximumRedirection 0 `
@@ -205,7 +205,7 @@ function create_authorization_cookie {
        --post-data="${AUTH_DATA}" `
        --user="${MOS_USERNAME}" `
        --password="${MOS_PASSWORD}" `
-       "https://updates.oracle.com/Orion/SimpleSearch/switch_to_saved_searches"  `
+       "https://updates.oracle.com/Orion/Services/download"  `
        --output-document="${AUTH_OUTPUT}" `
        --output-file="${AUTH_LOGFILE}"
 }
